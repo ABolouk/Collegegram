e3a229b3e68374c7a0061ea6d0376414d26043de
ABolouk <amirhosseinbolouk@gmail.com>
2023-08-25 23:28:09 +0330

    feat: Add forgetPassword & resetPassword routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,21 +2,36 @@ import { Router } from "express";
 import { loginDto } from '../modules/user/dto/login.dto';
 import { handleExpresss } from "../utility/handleExpress";
 import { UserService } from '../modules/user/userService';
+import { forgetPasswordDto } from "../modules/user/dto/forgetPassword.dto";
+import { BadRequestError } from "../utility/http-errors";
 import { signupDto } from "../modules/user/dto/signup.dto";
 
 
+export const resetPasswordRoute = "reset_password"
 
-
-export const makeUserRouter = (userService : UserService) => {
+export const makeUserRouter = (userService: UserService) => {
 	const app = Router();
 	app.post("/login", (req, res) => {
 		const dto = loginDto.parse(req.body);
-    handleExpresss(res, () => userService.login(dto));
+        handleExpresss(res, () => userService.login(dto));
 	});
 	
 	app.post("/register", (req, res) => {
 		const dto = signupDto.parse(req.body);
 		handleExpresss(res, () => userService.signup(dto), 201)
 	})
+
+	app.post("/login/forget", (req, res) => {
+		const dto = forgetPasswordDto.parse(req.body);
+		handleExpresss(res, () => userService.forgetPassword(dto));
+	})
+
+	app.post(`/${resetPasswordRoute}/:userId/:token`, async (req, res) => {
+		const { userId, token } = req.params;
+		const { password1, password2 } = req.body;
+
+		handleExpresss(res, () => userService.resetPassword(userId, token, password1, password2));
+	})
+
 	return app;
 };
