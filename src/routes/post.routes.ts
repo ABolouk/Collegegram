fb303c09d1e2610988857d0dacc124c5d723f6a0
ABolouk <amirhosseinbolouk@gmail.com>
2023-09-01 03:09:48 +0330

    feat: Complete post routes

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -4,24 +4,33 @@ import { createPostDto } from "../modules/post/dto/create-post.dto";
 import { handleExpresss } from "../utility/handleExpress";
 import { loginMiddle } from "../loginMiddle";
 import { UserService } from "../modules/user/userService";
+import { isPostId } from "../modules/post/model/post-id";
+import { BadRequestError } from "../utility/http-errors";
+import { getPostsDto } from "../modules/post/dto/get-posts-dto";
 
 export const makePostRouter = (userService: UserService, postService: PostService) => {
 	const app = Router();
-	app.post("/", async (req, res) => {
+	app.post("/", loginMiddle(userService), async (req, res) => {
 		const data = createPostDto.parse(req.body);
 		handleExpresss(res, () => postService.createPost(data));
 	});
 
-	app.get("/:id", loginMiddle(userService), (req, res) => {
+	app.get("/:id", loginMiddle(userService), async (req, res) => {
 		const { id } = req.params;
-
-
-		// handleExpresss(res, () => );
+		if (!Number.isInteger(id)) {
+			throw new BadRequestError('not a valid postId');
+		}
+		const postId = +(id);
+		if (!isPostId(postId)) {
+			throw new BadRequestError('not a valid postId');
+		}
+		handleExpresss(res, () => postService.getPostById(postId));
 	});
 
-	app.post("/user", (req, res) => {
-
-		// handleExpresss(res, () => );
+	app.post("/user", loginMiddle(userService), (req, res) => {
+		const user = req.user;
+		const { perPage, pageNumber } = getPostsDto.parse(req.body)
+		handleExpresss(res, () => postService.getPostsByUserId(user.id, perPage, pageNumber));
 	});
 
 	return app;
