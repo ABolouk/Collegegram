e269d2b619c02695cc6fe0cd92b5c3c8b75de9ce
sara yahyazadeh <sarayahyazadeh@saras-MacBook-Air.local>
2023-08-31 22:53:44 +0330

    feat: comment dao created

---
+++ src/modules/post/comment/dao/create-comment.dao.ts
@@ -0,0 +1,29 @@
+import { UserId } from "../../../user/model/user.id";
+import { PostId } from "../../model/post-id";
+import { CommentEntity } from "../entity/comment.entity";
+
+export interface CommentDao {
+  autherId: UserId;
+  postId: PostId;
+  content: string
+}
+
+export const createCommentDao = (comment: CommentEntity): CommentDao => ({
+  autherId: comment.userId,
+  postId: comment.postId,
+  content: comment.content
+})
+
+
+export const getCommentDao = (comment: CommentEntity | null) => {
+  if (comment) {
+    return {
+      autherId: comment.userId,
+      postId: comment.postId,
+      content: comment.content
+    }
+  }
+  else {
+    return null
+  }
+}
