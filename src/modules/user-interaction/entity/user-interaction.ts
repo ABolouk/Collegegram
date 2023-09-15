17613703d2b737141d36422e033ef9d505e1d866
sara yahyazadeh <sarayahyazadeh@saras-MacBook-Air.local>
2023-09-15 23:41:35 +0330

    refactor: UserInteractionEntiy refactored

--- src/modules/user-interaction/entity/user-interaction.ts
+++ src/modules/user-interaction/entity/user-interaction.ts
@@ -1,19 +1,21 @@
-import { CreateDateColumn, Entity, JoinTable, ManyToMany, PrimaryColumn, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinTable, ManyToMany, PrimaryColumn, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from "typeorm";
 import { UserEntity } from "../../user/entity/user.entity";
 
 @Entity("userInteraction")
-@Unique('unique1 interaction', ["userId1", "userId2"])
-@Unique('unique2 interaction', ["userId2", "userId1"])
+@Unique(["userId1", "userId2"])
+// @Unique(["userId2", "userId1"])
 export class UserInteractionEntity {
   @PrimaryGeneratedColumn()
-  id!: number 
+  id!: number
 
   @ManyToMany(() => UserEntity)
   @JoinTable()
+  @Column({name: "userId1"})
   userId1!: string
 
   @ManyToMany(() => UserEntity)
   @JoinTable()
+  @Column({ name: "userId2" })
   userId2!: string
 
   @CreateDateColumn()
