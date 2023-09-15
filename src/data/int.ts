9d7708fc2d39b4a88e82c8cdec964917f4ade40d
ABolouk <amirhosseinbolouk@gmail.com>
2023-09-15 16:15:26 +0330

    feat: Add int type

--- src/data/int.ts
+++ src/data/int.ts
@@ -2,8 +2,6 @@ import { Brand } from "../utility/brand";
 
 export type Int = Brand<number, "Int">;
 
-export const isInt = (x: number): x is Int => Number.isInteger(x);
-
-export type WholeNumber = Brand<Int, "WholeNumber">;
-
-export const isWholeNumber = (x: number): x is WholeNumber => isInt(x) && x >= 0;
+export module Int {
+    export const is = (x: number): x is Int => Number.isInteger(x);
+}
