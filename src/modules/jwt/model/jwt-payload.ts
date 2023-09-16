0d3347e1497c990f53cc737e5e255c83e97c440c
sara yahyazadeh <sarayahyazadeh@saras-MacBook-Air.local>
2023-09-16 23:15:17 +0330

    bug fix: jwt fixed

--- src/modules/jwt/model/jwt-payload.ts
+++ src/modules/jwt/model/jwt-payload.ts
@@ -3,5 +3,3 @@ import { UserId } from "../../user/model/user.id";
 export interface JwtLoginPayload {
   id: UserId // NOTE: ask about UserId
 }
-
-export type verifedToken = "valid" | "invalid" // NOTE: ask about it
