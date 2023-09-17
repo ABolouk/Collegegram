7c09fb767cfcb7fe0cb40529d1bae842932cd92a
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-09-17 17:47:45 +0330

    bug fix : fix cors credentials

--- src/api.ts
+++ src/api.ts
@@ -1,21 +1,22 @@
-import express, { ErrorRequestHandler } from "express";
-import { DataSource } from "typeorm";
-import { UserRepository } from "./modules/user/user.repository";
-import { UserService } from "./modules/user/user.service";
-import { makeUserRouter } from "./routes/user.routes";
-import { sessionRepository } from "./modules/user/session.repository";
-import { ZodError } from "zod";
-import { EmailService } from "./modules/email/email.service";
-import { makePostRouter } from "./routes/post.routes";
-import { PostRepository } from "./modules/post/post.repository";
-import { PostService } from "./modules/post/post.service";
-import { CommentService } from "./modules/post/comment/comment.service";
-import { CommentRepository } from "./modules/post/comment/comment.repository";
-import { JwtService } from "./modules/jwt/jwt.service";
+import express, {ErrorRequestHandler} from "express";
+import {DataSource} from "typeorm";
+import {UserRepository} from "./modules/user/user.repository";
+import {UserService} from "./modules/user/user.service";
+import {makeUserRouter} from "./routes/user.routes";
+import {sessionRepository} from "./modules/user/session.repository";
+import {ZodError} from "zod";
+import {EmailService} from "./modules/email/email.service";
+import {makePostRouter} from "./routes/post.routes";
+import {PostRepository} from "./modules/post/post.repository";
+import {PostService} from "./modules/post/post.service";
+import {CommentService} from "./modules/post/comment/comment.service";
+import {CommentRepository} from "./modules/post/comment/comment.repository";
+import {JwtService} from "./modules/jwt/jwt.service";
 import cors from 'cors'
+
 export const makeApp = (dataSource: DataSource) => {
     const app = express();
-    app.use(cors({ origin: '*' }))
+    app.use(cors({credentials: true, origin: '*'}))
     app.use(express.json())
     const userRepo = new UserRepository(dataSource);
     const sessionRepo = new sessionRepository(dataSource);
@@ -41,9 +42,9 @@ export const makeApp = (dataSource: DataSource) => {
         next,
     ) => {
         if (error instanceof ZodError) {
-            res.status(400).send({ message: error.issues });
+            res.status(400).send({message: error.issues});
         }
-        res.status(500).send({ message: "خطایی رخ داده است. لطفا دوباره تلاش کنید." });
+        res.status(500).send({message: "خطایی رخ داده است. لطفا دوباره تلاش کنید."});
     };
     app.use(zodErrorHanlder);
     return app
