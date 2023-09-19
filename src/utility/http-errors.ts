18a2af1b2eabf1eed2d598c9f2efcf9ba132057f
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-09-19 19:11:44 +0330

    feat :  add Request Not found

--- src/utility/http-errors.ts
+++ src/utility/http-errors.ts
@@ -1,17 +1,23 @@
 export class HttpError {
-	constructor(public status: number, public message: string ) {  // behtare parameter type ezafe shavad
-	}
-}
-
-
-type Fields = 'User' | 'Post' | 'Email'
-const translate = (field: Fields) => {
-    switch (field) {
-        case 'Post': return 'پست'
-        case 'User': return 'کاربر'
-        case 'Email': return 'ایمیل'
+    constructor(public status: number, public message: string) {  // behtare parameter type ezafe shavad
     }
 }
+
+
+type Fields = 'User' | 'Post' | 'Email' | 'Request'
+const translate = (field: Fields) => {
+    switch (field) {
+        case 'Post':
+            return 'پست'
+        case 'User':
+            return 'کاربر'
+        case 'Email':
+            return 'ایمیل'
+        case 'Request':
+            return 'درخواست'
+    }
+}
+
 export class NotFoundError extends HttpError {
     constructor(field: Fields) {
         super(404, `${translate(field)} یافت نشد.`);
@@ -19,10 +25,11 @@ export class NotFoundError extends HttpError {
 }
 
 export class ForbiddenError extends HttpError {
-	constructor() {
-		super(403, "شما از سیستم خارج شده اید. لطفا دوباره وارد شوید.");
-	}
+    constructor() {
+        super(403, "شما از سیستم خارج شده اید. لطفا دوباره وارد شوید.");
+    }
 }
+
 export class UnauthorizedError extends HttpError {
     constructor() {
         super(401, "شما اجازه دسترسی به این صفحه را ندارید.");
@@ -39,10 +46,10 @@ export class serviceUnavailableError extends HttpError {
     constructor() {
         super(503, "سرور در دسترس نیست.");
     }
-    
+
 }
 
-export class ConflictError extends HttpError{
+export class ConflictError extends HttpError {
     constructor(message: string) {
         super(409, message)
     }
