f66d2b8a7139ad607fbf8c558c4176af460d93c9
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-09-19 19:13:38 +0330

    feat : add followReq get and update  func in repo

--- src/modules/follow/follow-request.repository.ts
+++ src/modules/follow/follow-request.repository.ts
@@ -1,16 +1,34 @@
-import { DataSource, Repository } from "typeorm";
-import { FollowRequestEntity } from "./entity/follow-request.entity";
-import { createFollowRequest, followReqIdDao } from "./model/follow.request";
-import { zodFollowReqIdDao } from "./dao/follow.req.dao";
+import {DataSource, Repository} from "typeorm";
+import {FollowRequestEntity} from "./entity/follow-request.entity";
+import {createFollowRequest, followReqDao, followReqIdDao, FollowRequest} from "./model/follow.request";
+import {zodFollowReqDao, zodFollowReqIdDao} from "./dao/follow.req.dao";
+import {FollowReqStatus} from "./model/follow.req.status";
+import status = FollowReqStatus.status;
+import {FollowReqId} from "./model/follow.req.id";
 
-export class FollowRequestRepository {
+export class followRequestRepository {
     private followRequestRepo: Repository<FollowRequestEntity>;
+
     constructor(appDataSource: DataSource) {
         this.followRequestRepo = appDataSource.getRepository(FollowRequestEntity);
     }
 
     async createFollowRequest(followRequest: createFollowRequest): Promise<followReqIdDao> {
         return this.followRequestRepo.save(followRequest).then((x) => zodFollowReqIdDao.parse(x));
+    }
+
+    async getFollowRequest(followRequest: FollowRequest): Promise<followReqDao | null> {
+        return this.followRequestRepo.find({
+            where: {
+                followerId: followRequest.followerUserId,
+                followingId: followRequest.followingUserId
+            }
+        }).then((x) => x ? zodFollowReqDao.parse(x) : null);
+    }
+
+    async updateFollowRequest(followReqId: FollowReqId, followReqStatus: FollowReqStatus.status) {
+        await this.followRequestRepo.update({id: followReqId}, {status: followReqStatus});
+        return {status: "updated"};
     }
 
 }
