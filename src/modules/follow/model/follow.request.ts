fe1b62de409e37e339dca475feb6356423be3c2b
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-09-19 19:11:29 +0330

    feat :  add  FollowReqDao

--- src/modules/follow/model/follow.request.ts
+++ src/modules/follow/model/follow.request.ts
@@ -1,13 +1,13 @@
-import { v4 } from "uuid";
-import { UserId } from "../../user/model/user.id";
-import { UserName } from "../../user/model/user.username";
-import { FollowReqId } from "./follow.req.id";
-import { FollowReqStatus } from "./follow.req.status";
-import { User } from '../../user/model/user';
+import {v4} from "uuid";
+import {UserId} from "../../user/model/user.id";
+import {UserName} from "../../user/model/user.username";
+import {FollowReqId} from "./follow.req.id";
+import {FollowReqStatus} from "./follow.req.status";
+import {User} from '../../user/model/user';
 
 export interface FollowRequest {
-    followerUserId: UserName;
-    followingUserId: UserName;
+    followerUserId: UserId;
+    followingUserId: UserId;
 }
 
 export interface createFollowRequest {
@@ -19,4 +19,12 @@ export interface createFollowRequest {
 
 export interface followReqIdDao {
     id: FollowReqId;
+}
+
+export interface followReqDao {
+    id: FollowReqId;
+    interactionId: number;
+    followerId: UserId;
+    followingId: UserId;
+    status: FollowReqStatus;
 }
