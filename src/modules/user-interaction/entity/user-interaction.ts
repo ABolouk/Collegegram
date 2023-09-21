64b9b8cff4b6908e355575910ef0d02fa274f204
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-09-22 00:15:00 +0330

    bug fix : just fix tabs

--- src/modules/user-interaction/entity/user-interaction.ts
+++ src/modules/user-interaction/entity/user-interaction.ts
@@ -1,26 +1,36 @@
-import { Column, CreateDateColumn, Entity, JoinTable, ManyToMany, PrimaryColumn, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from "typeorm";
-import { UserEntity } from "../../user/entity/user.entity";
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinTable,
+    ManyToMany,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    Unique,
+    UpdateDateColumn
+} from "typeorm";
+import {UserEntity} from "../../user/entity/user.entity";
 
 @Entity("userInteraction")
 @Unique(["userId1", "userId2"])
 // @Unique(["userId2", "userId1"])
 export class UserInteractionEntity {
-  @PrimaryGeneratedColumn()
-  id!: number
+    @PrimaryGeneratedColumn()
+    id!: number
 
-  @ManyToMany(() => UserEntity)
-  @JoinTable()
-  @Column({name: "userId1"})
-  userId1!: string
+    @ManyToMany(() => UserEntity)
+    @JoinTable()
+    @Column({name: "userId1"})
+    userId1!: string
 
-  @ManyToMany(() => UserEntity)
-  @JoinTable()
-  @Column({ name: "userId2" })
-  userId2!: string
+    @ManyToMany(() => UserEntity)
+    @JoinTable()
+    @Column({name: "userId2"})
+    userId2!: string
 
-  @CreateDateColumn()
-  createdAt!: Date;
+    @CreateDateColumn()
+    createdAt!: Date;
 
-  @UpdateDateColumn()
-  updatedAt!: Date;
+    @UpdateDateColumn()
+    updatedAt!: Date;
 }
