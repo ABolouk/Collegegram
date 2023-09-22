27ca6249e92b638ecdc847923d99831797b7ed2e
ABolouk <amirhosseinbolouk@gmail.com>
2023-09-23 02:33:20 +0330

    feat: Add MinIO upload middleware

--- src/utility/multer.ts
+++ src/utility/multer.ts
@@ -1,4 +1,6 @@
 import multer from 'multer';
+import * as minio from 'minio';
+import multerMinIOStorage from 'multer-minio-storage';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -17,3 +19,22 @@ const multipleStorage = multer.diskStorage({
     }
 })
 export const uploadMultiple = multer({ storage: multipleStorage });
+
+const minioClient = new minio.Client({
+    endPoint: 'collegegrammedia.darkube.app',
+    accessKey: 'HfelzkUMdJFYpmFtISkcA2Q8OVmo9C33',
+    secretKey: 'lx3TGY1OiRXdeAs0Fp8QBvYvFdgsoqQG',
+});
+
+const minIOStorage = multerMinIOStorage({
+    minioClient: minioClient,
+    bucket: 'mediacollegegram',
+    metadata: function (req, file, cb) {
+        cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+        cb(null, Date.now().toString() + '-' + file.originalname)
+    }
+})
+
+export const uploadMinIO = multer({storage : minIOStorage})
