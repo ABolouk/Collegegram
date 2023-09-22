6da4e568b153bc6fe3c336281faac2e61a68c125
ABolouk <amirhosseinbolouk@gmail.com>
2023-09-22 23:02:04 +0330

    bugfix: Change getPostDto

--- src/modules/post/dto/get-posts-dto.ts
+++ src/modules/post/dto/get-posts-dto.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const getPostsDto = z.object({
-    limit: z.number().nonnegative(),
-    nextOffset: z.number().nonnegative().nullable(),
+    limit: z.coerce.number().nonnegative(),
+    nextOffset: z.coerce.number().nonnegative().nullable(),
 })
 
 export type GetPostsDto = z.infer<typeof getPostsDto>;
