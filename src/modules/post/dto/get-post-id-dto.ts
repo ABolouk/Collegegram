2691dded381a4d288925e82af902f140f6bdf83c
ABolouk <amirhosseinbolouk@gmail.com>
2023-09-24 17:00:02 +0330

    bugfix: change getPostIdDto

--- src/modules/post/dto/get-post-id-dto.ts
+++ src/modules/post/dto/get-post-id-dto.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { PostId } from "../model/post-id";
 
 export const getPostIdDto = z.object({
-    postId: z.number().refine(PostId.is),
+    id: z.coerce.number().refine(PostId.is),
 })
 
 export type GetPostIdDto = z.infer<typeof getPostIdDto>;
