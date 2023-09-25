b1247dd74c4b4c180dbd43e13a3c1180cc409b4c
sara yahyazadeh <sarayahyazadeh@saras-MacBook-Air.local>
2023-09-25 21:53:55 +0330

    refactor: delete comment code

--- src/modules/post/tag/dto/tag.dto.ts
+++ src/modules/post/tag/dto/tag.dto.ts
@@ -5,31 +5,6 @@ import { CreateTagInterface, SplittedTag } from "../model/tag";
 import { TagTitle } from "../model/tag-title";
 import { Brand } from "../../../../utility/brand";
 
-// export const tagDto = z.object({
-//     title: z.string().min(2).max(7),
-//     color: z.string().refine(HexadecimalColor.is),
-// })
-
-// export interface TagDto extends z.infer<typeof tagDto> { }
-
-// const splitStringtoArray = (str: string): SplittedTag[] => {
-//     return str.split(' ').map((value) => ({ title: value, color: randomHexaDecimalColor() }))
-// }
-
-// const isTags = (tags: SplittedTag[]): boolean => {
-//     return tags
-//         .map((x) => x.title.length >= 2 && x.title.length <= 7 && HexadecimalColor.is(x.color))
-//         .every(val => val === true);
-// }
-
-// const changeStringArrayToTags = (tags: SplittedTag[]): CreateTagInterface[] => {
-//     return tags.map((x) => ({ title: x.title as TagTitle, color: x.color as HexadecimalColor }))
-// };
-// export const zodTags = z.string().transform(splitStringtoArray).refine(isTags).transform(changeStringArrayToTags);
-
-
-// export type Tag = Brand<{ title: TagTitle, color: HexadecimalColor }, "Tag">
-
 
 export interface Tag {
     title: TagTitle,
