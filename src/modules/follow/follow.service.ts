2d5346bb0b32889c5d5d924b892c4916be8a4fbb
sara yahyazadeh <sarayahyazadeh@saras-MacBook-Air.local>
2023-09-27 01:45:04 +0330

    feat: blockEvent.on added to followService

--- src/modules/follow/follow.service.ts
+++ src/modules/follow/follow.service.ts
@@ -1,26 +1,30 @@
-import {FollowRepository} from "./follow.repository";
-import {createFollowRelation, Follow} from "./model/follow";
-import {ConflictError} from "../../utility/http-errors";
+import { FollowRepository } from "./follow.repository";
+import { createFollowRelation, Follow } from "./model/follow";
+import { ConflictError } from "../../utility/http-errors";
+import { blockEventEmmmiter } from "../../utility/event-handling";
+
 
 export class followService {
     constructor(private followRepo: FollowRepository) {
+        blockEventEmmmiter.on("block", (x) => this.deleteFollowRelation(x))
     }
+
 
     async getFollowRelation(followRelation: Follow) {
         return this.followRepo.getFollowRelation(followRelation);
     }
 
     async createFollowRelation(followRelation: createFollowRelation) {
-        const followRell = await this.getFollowRelation({followerId: followRelation.followerId, followingId: followRelation.followingId});
+        const followRell = await this.getFollowRelation({ followerId: followRelation.followerId, followingId: followRelation.followingId });
         if (followRell) {
             throw new ConflictError("You are already following this user");
         }
         await this.followRepo.createFollowRelation(followRelation);
-        return {status: "followed"};
+        return { status: "followed" };
     }
 
     async deleteFollowRelation(followRelation: Follow) {
         await this.followRepo.deleteFollowRelation(followRelation);
-        return {status: "unfollowed"};
+        return { status: "unfollowed" };
     }
 }
