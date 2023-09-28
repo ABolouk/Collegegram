f2131eee13aff6ccf361f80a26343e7476ef69bf
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-09-28 17:46:56 +0330

    feat : add likeRepo and likeService in api

--- src/api.ts
+++ src/api.ts
@@ -19,6 +19,8 @@ import {JwtService} from "./modules/jwt/jwt.service";
 import cors from 'cors'
 import {UserInteractionRepository} from "./modules/user-interaction/user-interaction.repository";
 import {USerInteractionService} from "./modules/user-interaction/user-interaction.service";
+import {LikeRepository} from "./modules/post/like/like.repository";
+import {LikeService} from "./modules/post/like/like.service";
 
 
 export const makeApp = (dataSource: DataSource) => {
@@ -45,7 +47,9 @@ export const makeApp = (dataSource: DataSource) => {
     const postService = new PostService(postRepo);
     const commentRepo = new CommentRepository(dataSource);
     const commentService = new CommentService(commentRepo, postService);
-    app.use("/post", makePostRouter(userService, postService, commentService));
+    const likeRepo = new LikeRepository(dataSource);
+    const likeService = new LikeService(likeRepo, postService, userService, followRellService);
+    app.use("/post", makePostRouter(userService, postService, commentService, likeService));
 
     app.use((req, res, next) => {
         next();
