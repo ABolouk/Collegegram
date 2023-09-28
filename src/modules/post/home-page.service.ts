d294b6d6415f951b9756af4f7a656cdcf191c243
sara yahyazadeh <sarayahyazadeh@saras-MacBook-Air.local>
2023-09-29 00:15:28 +0330

    feat: create homepage service

---
+++ src/modules/post/home-page.service.ts
@@ -0,0 +1,17 @@
+import { PostService } from "./post.service";
+import { UserService } from "../user/user.service";
+import { homePageDtoType } from "./dto/home-page.dto";
+
+export class HomePageService {
+  constructor(private postService: PostService, private userService: UserService) { }
+  
+  
+
+  async getHome(dto: homePageDtoType) {
+    const followingUsersId = ["24e11db6-04d9-4f84-8835-aa78dd4e92d1"]
+    const posts = await this.postService.getPostsByUsersId(followingUsersId, dto.limit, dto.startTime)
+    const homePagePosts = await Promise.all(posts.map(async (x) => ({ id: x.id, familyName: await this.userService.getFamilyNameById(x.userId), tags: x.tags, photos: x.photos })))
+    return homePagePosts
+  }
+
+}
