c49d5a1d4d1288c032093804920dcce70f183ca8
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-09-28 17:49:04 +0330

    feat : add likeRepo but it is not complete

---
+++ src/modules/post/like/like.repository.ts
@@ -0,0 +1,23 @@
+import {DataSource, Repository} from "typeorm";
+import {LikeEntity} from "./entity/like.entity";
+import {zodLike} from "./dao/like.dao";
+import {likeInterface} from "./model/like";
+
+export class LikeRepository {
+    private likeRepo: Repository<LikeEntity>;
+
+    constructor(appDataSource: DataSource) {
+        this.likeRepo = appDataSource.getRepository(LikeEntity);
+    }
+    //TODO: better name
+    async create(like: likeInterface): Promise<likeInterface> {
+        return this.likeRepo.save(like).then((x) => zodLike.parse(x));
+    }
+
+    async isLiked(like: likeInterface): Promise<likeInterface | null> {
+        return this.likeRepo.findOneBy({userId: like.userId, postId: like.postId});
+    }
+
+
+
+}
