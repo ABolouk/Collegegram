e2ceb9d95d9f888798fdfadd76b7ca1fce9d1a45
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-09-29 12:01:47 +0330

    feat : add tranaction for counting

--- src/modules/post/like/like.repository.ts
+++ src/modules/post/like/like.repository.ts
@@ -2,26 +2,51 @@ import {DataSource, Repository} from "typeorm";
 import {LikeEntity} from "./entity/like.entity";
 import {zodLike} from "./dao/like.dao";
 import {likeInterface} from "./model/like";
+import {PostEntity} from "../entity/post.entity";
 
 export class LikeRepository {
     private likeRepo: Repository<LikeEntity>;
 
-    constructor(appDataSource: DataSource) {
+    constructor(private appDataSource: DataSource) {
         this.likeRepo = appDataSource.getRepository(LikeEntity);
     }
+
     //TODO: better name
     async create(like: likeInterface): Promise<likeInterface> {
-        return this.likeRepo.save(like).then((x) => zodLike.parse(x));
+        return new Promise<likeInterface>(async (resolve, reject) => {
+            try {
+                await this.appDataSource.manager.transaction(async manager => {
+                    const postRepo = manager.getRepository(PostEntity);
+                    const likeRepo = manager.getRepository(LikeEntity);
+                    const newLike = await likeRepo.save({
+                        userId: like.userId,
+                        postId: like.postId
+                    });
+                    await postRepo.update(
+                        {id: like.postId},
+                        {likeCount: () => "likeCount + 1"}
+                    );
+                    resolve(newLike);
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
 
     async isLiked(like: likeInterface): Promise<likeInterface | null> {
         return this.likeRepo.findOneBy({userId: like.userId, postId: like.postId});
     }
 
-    async delete(like: likeInterface): Promise<void> {
-        await this.likeRepo.delete({userId: like.userId, postId: like.postId});
+    async delete(like: likeInterface) {
+        await this.appDataSource.manager.transaction(async manager => {
+            const postRepo = manager.getRepository(PostEntity);
+            const likeRepo = manager.getRepository(LikeEntity);
+            const newLike = await likeRepo.delete({userId: like.userId, postId: like.postId});
+            await postRepo.update(
+                {id: like.postId},
+                {likeCount: () => "likeCount - 1"}
+            );
+        });
     }
-
-
-
 }
