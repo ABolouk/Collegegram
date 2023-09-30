755238ebab6bc7dede99940f3b336988581543c3
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-01 02:11:12 +0330

    feat : add blockAction and better create

--- src/modules/post/like/like.repository.ts
+++ src/modules/post/like/like.repository.ts
@@ -1,8 +1,10 @@
-import {DataSource, Repository} from "typeorm";
+import {DataSource, In, Repository} from "typeorm";
 import {LikeEntity} from "./entity/like.entity";
 import {zodLike} from "./dao/like.dao";
 import {likeInterface} from "./model/like";
 import {PostEntity} from "../entity/post.entity";
+import {UserId} from "../../user/model/user.id";
+import {PostId} from "../model/post-id";
 
 export class LikeRepository {
     private likeRepo: Repository<LikeEntity>;
@@ -13,24 +15,18 @@ export class LikeRepository {
 
     //TODO: better name
     async create(like: likeInterface): Promise<likeInterface> {
-        return new Promise<likeInterface>(async (resolve, reject) => {
-            try {
-                await this.appDataSource.manager.transaction(async manager => {
-                    const postRepo = manager.getRepository(PostEntity);
-                    const likeRepo = manager.getRepository(LikeEntity);
-                    const newLike = await likeRepo.save({
-                        userId: like.userId,
-                        postId: like.postId
-                    });
-                    await postRepo.update(
-                        {id: like.postId},
-                        {likeCount: () => "likeCount + 1"}
-                    );
-                    resolve(newLike);
-                });
-            } catch (error) {
-                reject(error);
-            }
+        return await this.appDataSource.manager.transaction(async manager => {
+            const postRepo = manager.getRepository(PostEntity);
+            const likeRepo = manager.getRepository(LikeEntity);
+            const newLike = await likeRepo.save({
+                userId: like.userId,
+                postId: like.postId
+            });
+            await postRepo.update(
+                {id: like.postId},
+                {likeCount: () => "likeCount + 1"}
+            );
+            return zodLike.parse(newLike);
         });
     }
 
@@ -49,4 +45,45 @@ export class LikeRepository {
             );
         });
     }
+
+    async blockedUserLike(dto: { blockerId: UserId, blockedId: UserId }) {
+        const like = await this.likeRepo.find({
+            select: {
+                userId: true,
+                postId: true,
+                post: {
+                    userId: true,
+                },
+            },
+            relations: {
+                post: true,
+            },
+            where: [{
+                userId: dto.blockerId,
+                post: {
+                    userId: dto.blockedId,
+                }
+            }, {
+                userId: dto.blockedId,
+                post: {
+                    userId: dto.blockerId,
+                }
+            },
+            ],
+        });
+        await this.appDataSource.manager.transaction(async manager => {
+            const postRepo = manager.getRepository(PostEntity);
+            const likeRepo = manager.getRepository(LikeEntity);
+            const newLike = await likeRepo.delete(
+                {postId: In(like.map(x => x.postId))}
+            );
+            await postRepo.update(
+                {id: In(like.map(x => x.postId))},
+                {likeCount: () => "likeCount - 1"}
+            );
+        });
+        return {status: "blocked"}
+    }
+
+
 }
