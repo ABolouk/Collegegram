d5e4fc707c101457a8462f50e31eaeeb082fb1f2
sara yahyazadeh <sarayahyazadeh@saras-MacBook-Air.local>
2023-09-30 15:19:12 +0330

    delete comments

--- src/modules/post/model/home-page.ts
+++ src/modules/post/model/home-page.ts
@@ -27,15 +27,11 @@ export interface HomePagePost {
 export module HomePagePost {
   export const zod = z.object({
     id: PostId.zod,
-    UserId: UserId.zod,
+    userId: UserId.zod,
     tags: z.array(Tag.zod),
     photos: z.array(z.string())
   })
 }
-
-// export interface HomePagePosts{
-//   HomePagePosts : HomePagePost[]
-// }
 
 export type HomePagePosts = HomePagePost[]
 
@@ -43,13 +39,4 @@ export type HomePagePosts = HomePagePost[]
 export module HomePagePosts {
   export const zod = z.array(HomePagePost.zod)
 }
-
-
-
-// export namespace FollowingUserOrNaot {
-//   const isFollowingUser = (value: User, homeUser: UserHomePage): value is FollowingUser {
-//     return homeUser.followingUsers.map((x: FollowingUser) => ({x.UserName})) 
-//   }
-//   const userName : UserName
-// }
 
