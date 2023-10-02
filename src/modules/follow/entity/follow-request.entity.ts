0eb04048822ccc95aa9e0224b9fbc01e593d3cb5
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-02 19:09:52 +0330

    refactor : delete UserInteraction

--- src/modules/follow/entity/follow-request.entity.ts
+++ src/modules/follow/entity/follow-request.entity.ts
@@ -11,19 +11,11 @@ import {UserId} from "../../user/model/user.id";
 import {FollowReqId} from "../model/follow.req.id";
 import {FollowReqStatus} from "../model/follow.req.status";
 import {UserEntity} from "../../user/entity/user.entity";
-import {UserInteractionEntity} from "../../user-interaction/entity/user-interaction";
 
 @Entity("follow requset")
 export class FollowRequestEntity {
     @PrimaryGeneratedColumn()
     id!: FollowReqId;
-
-    @Column()
-    interactionId!: number;
-
-    @ManyToOne(() => UserInteractionEntity, {onDelete: "CASCADE"})
-    interaction!: UserInteractionEntity;
-
 
     @Column()
     followerId!: UserId;
