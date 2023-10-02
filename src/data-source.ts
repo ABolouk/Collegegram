501edc5fc913528c7d543b4f04f9c9f01932de7f
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-02 19:14:18 +0330

    refactor : delete UserInterAction

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,6 @@ import 'dotenv-flow/config';
 import { PostEntity } from "./modules/post/entity/post.entity";
 import { CommentEntity } from "./modules/post/comment/entity/comment.entity";
 import { TagEntity } from "./modules/post/tag/entity/tag.entity";
-import { UserInteractionEntity } from "./modules/user-interaction/entity/user-interaction";
 import { BlockEntity } from "./modules/block/entity/block.entity";
 import { FollowEntity } from "./modules/follow/entity/follow.entity";
 import { FollowRequestEntity } from "./modules/follow/entity/follow-request.entity";
@@ -19,7 +18,7 @@ export const AppDataSource = new DataSource({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     logging: false,
-    entities: [UserEntity, SessionEntity, PostEntity, CommentEntity, TagEntity, UserInteractionEntity, FollowEntity, FollowRequestEntity, BlockEntity],
+    entities: [UserEntity, SessionEntity, PostEntity, CommentEntity, TagEntity, FollowEntity, FollowRequestEntity, BlockEntity],
     migrations: ["./src/migrations/*.ts"],
     subscribers: [],
 });
