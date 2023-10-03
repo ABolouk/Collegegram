6a3710cca34dd6ead9c186f7621e880c6c602488
ABolouk <amirhosseinbolouk@gmail.com>
2023-10-04 01:26:22 +0330

    feat: Add NotificationPost

---
+++ src/modules/notification/model/notification-post.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { PostId } from "../../post/model/post-id";
+
+export interface NotificationPost {
+    id: PostId;
+    photo: string;
+}
+
+export module NotificationPost {
+    export const zod = z.object({
+        id: PostId.zod,
+        photo: z.string(),
+    })
+}
