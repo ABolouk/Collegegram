014efebea3453ae43612e24a6b3a9a9dd033145d
ABolouk <amirhosseinbolouk@gmail.com>
2023-10-04 01:41:19 +0330

    feat: Add getNotificationsByUserId method to NotificationRepo

--- src/modules/notification/notification.repository.ts
+++ src/modules/notification/notification.repository.ts
@@ -4,6 +4,7 @@ import { NotificationEntity } from "./entity/notification.entity";
 import { NotificationId } from "./model/notification-id";
 import { NotificationInterface } from "./model/notification";
 import { zodNotificationDao } from "./dao/notification.dao";
+import {UserId} from "../user/model/user.id";
 
 export class NotificationRepository {
     private notificationRepo: Repository<NotificationEntity>;
@@ -17,5 +18,10 @@ export class NotificationRepository {
             relations: ['users', 'posts', 'comments'],
             where: { id: id },
         }).then((x) => z.nullable(zodNotificationDao).parse(x));
+    }
+
+    async getNotificationsByUserId(id: UserId): Promise<NotificationInterface[]> {
+        return await this.notificationRepo.findBy({interactedUserId: id})
+            .then(notifications => notifications.map(x => zodNotificationDao.parse(x)))
     }
 }
