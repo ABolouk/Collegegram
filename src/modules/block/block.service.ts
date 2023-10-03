77945cbe9ea4b0a3b4afd9831f936306f2ac6245
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-03 23:27:44 +0330

    feat : add event and emit it

--- src/modules/block/block.service.ts
+++ src/modules/block/block.service.ts
@@ -1,42 +1,48 @@
-import { NotFoundError } from "../../utility/http-errors";
-import { UserId } from "../user/model/user.id";
-import { UserService } from "../user/user.service";
-import { BlockRepository } from "./block.repository";
-import { BlockDtoType } from "./dto/block.dto";
+import {NotFoundError} from "../../utility/http-errors";
+import {UserId} from "../user/model/user.id";
+import {UserService} from "../user/user.service";
+import {BlockRepository} from "./block.repository";
+import {BlockDtoType} from "./dto/block.dto";
+import {blockEventEmmmiter} from "../../data/event-handling";
 
 export class BlockService {
-  constructor(private blockRepo: BlockRepository, private userService: UserService) { }
-
-  async block(dto: BlockDtoType) {
-    const blockedUserId = await this.userService.getUserIdByUserName(dto.blockedUserName)
-
-    if (!blockedUserId) {
-      throw new NotFoundError("User")
-    }
-    const block = {
-      userId: dto.userId,
-      blockedUserId: blockedUserId
+    constructor(private blockRepo: BlockRepository, private userService: UserService) {
     }
 
-    await this.blockRepo.block(block)
+    async block(dto: BlockDtoType) {
+        const blockedUserId = await this.userService.getUserIdByUserName(dto.blockedUserName)
 
-  }
+        if (!blockedUserId) {
+            throw new NotFoundError("User")
+        }
+        const block = {
+            userId: dto.userId,
+            blockedUserId: blockedUserId
+        }
 
-  async unblock(dto: BlockDtoType) {
-    const blockedUserId = await this.userService.getUserIdByUserName(dto.blockedUserName)
-    if (!blockedUserId) {
-      throw new NotFoundError("User")
+        await this.blockRepo.block(block)
+
+        blockEventEmmmiter.emit("block", block.userId, block.blockedUserId)
+
+        return { status: "Blocked" }
+
     }
-    const deletedBlock = {
-      userId: dto.userId,
-      blockedUserId: blockedUserId
-    }
-    return this.blockRepo.findBlock(deletedBlock)
-  }
 
-  async getBlockedUsers(id: UserId) {
-    const blockedUsers = await this.blockRepo.findBlockedUsers(id)
-    return blockedUsers
-  }
+    async unblock(dto: BlockDtoType) {
+        const blockedUserId = await this.userService.getUserIdByUserName(dto.blockedUserName)
+        if (!blockedUserId) {
+            throw new NotFoundError("User")
+        }
+        const deletedBlock = {
+            userId: dto.userId,
+            blockedUserId: blockedUserId
+        }
+        return this.blockRepo.findBlock(deletedBlock)
+    }
+
+    async getBlockedUsers(id: UserId) {
+        const blockedUsers = await this.blockRepo.findBlockedUsers(id)
+        return blockedUsers
+    }
 
 }
