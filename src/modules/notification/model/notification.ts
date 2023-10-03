4559b5e73cac41a0f7035c2cadd8422861ba852e
ABolouk <amirhosseinbolouk@gmail.com>
2023-10-04 01:32:29 +0330

    feat: Add NotificationInterface

--- src/modules/notification/model/notification.ts
+++ src/modules/notification/model/notification.ts
@@ -6,6 +6,9 @@ import {
     FollowRequestAcceptedNotificationType,
     FollowRequestPendingNotificationType
 } from "./follow-request-notification";
+import { NotificationUser } from "./notification-user";
+import { NotificationPost } from "./notification-post";
+import { NotificationComment } from "./notification-comment";
 
 const allNotificationTypes = [
     CommentNotificationType,
@@ -26,4 +29,13 @@ export module NotificationType {
     export const is = (value: string): value is NotificationType => allNotificationTypes.some(type => type.is(value));
 
     export const zod = z.string().refine(is);
+}
+
+export interface NotificationInterface {
+    interactingUser: NotificationUser;
+    interactedUser: NotificationUser;
+    type: NotificationType;
+    post?: NotificationPost;
+    comment?: NotificationComment;
+    updatedAt: Date;
 }
