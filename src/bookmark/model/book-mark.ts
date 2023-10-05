4a5fb9108bf97cb582b6f3beea75b4dfcd506eb9
sara yahyazadeh <sarayahyazadeh@saras-MacBook-Air.local>
2023-10-05 21:48:30 +0330

    feat: book mark interface added

--- src/bookmark/model/book-mark.ts
+++ src/bookmark/model/book-mark.ts
@@ -1,3 +1,4 @@
+import {z} from "zod"
 import { PostId } from "../../modules/post/model/post-id";
 import { UserId } from "../../modules/user/model/user.id";
 import { BookMarkId } from "./book-mark-id";
@@ -12,3 +13,23 @@ export interface bookmarkInterface {
   userId: UserId,
   postId: PostId
 }
+
+export type bookmarkPosts = bookmarkPostInterface[]
+
+export interface bookmarkPostInterface {
+  id: PostId
+  photos: string[]
+  createdAt: Date
+}
+export module bookmarkPost {
+  export const zod = z.object({
+    id: PostId.zod,
+    photos: z.array(z.string()),
+    createdAt: z.date()
+  })
+}
+
+export module bookmarkPosts {
+  export const zod = z.array(bookmarkPost.zod)
+}
+
