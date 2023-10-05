d93569898ddc17c20879c419e93fd42248314a1d
sara yahyazadeh <sarayahyazadeh@saras-MacBook-Air.local>
2023-10-05 21:53:38 +0330

    feat: get bookmark added

--- src/bookmark/book-mark.service.ts
+++ src/bookmark/book-mark.service.ts
@@ -4,8 +4,10 @@ import { BadRequestError, NotFoundError } from "../utility/http-errors";
 import { BookMarkDtoType } from "./dto/create-book-mark.dto";
 import { followService } from "../modules/follow/follow.service";
 import { BookmarkRepository } from "./book-mark.repository";
+import { GetBookMarkDtoType } from "./dto/get-book-mark.dto";
 
-export class BookMarkService {
+
+export class BookmarkService {
 
   constructor(private bookmarkRepo: BookmarkRepository, private postService: PostService, private userService: UserService, private followService: followService) { }
 
@@ -15,7 +17,6 @@ export class BookMarkService {
     if (!post) {
       throw new NotFoundError('Post');
     }
-
     const author = await this.userService.getUserById(dto.userId);
     if (author.isPrivate) {
       const follow = await this.followService.getFollowRelation({
@@ -59,6 +60,22 @@ export class BookMarkService {
 
     await this.bookmarkRepo.deleteBookmark(bookmark)
     return { status: "unbookmarked" }
+
+  }
+
+  async getBookmarks(dto: GetBookMarkDtoType) {
+    const result = await this.bookmarkRepo.getBookmarksByUserId(dto.userId, dto.limit, dto.startTime);
+    if (!result.bookmarkPosts) {
+      throw new NotFoundError("Post")
+    }
+    const bookmarkPosts = result.bookmarkPosts
+    const nextOffset = bookmarkPosts.length === 0 ? new Date() : bookmarkPosts[bookmarkPosts.length - 1].createdAt;
+    const hasMore = result.hasMore
+    return {
+      posts: bookmarkPosts,
+      nextOffset: nextOffset,
+      hasMore: hasMore,
+    }
 
   }
 }
