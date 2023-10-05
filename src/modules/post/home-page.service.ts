85f76f8366917f84c7175d268a5422929270cad4
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-05 23:53:48 +0330

    refactor : fix usage service

--- src/modules/post/home-page.service.ts
+++ src/modules/post/home-page.service.ts
@@ -1,11 +1,12 @@
-import { PostService } from "./post.service";
-import { UserService } from "../user/user.service";
+import { PostHighService } from "./post.high.service";
+import { UserHighService } from "../user/user.high.service";
 import { homePageDtoType } from "./dto/home-page.dto";
 import { FollowHighService } from "../follow/follow.high.service";
 import {FollowLowService} from "../follow/follow.low.service";
+import {UserLowService} from "../user/user.low.service";
 
 export class HomePageService {
-  constructor(private postService: PostService, private userService: UserService, private followLowService : FollowLowService) { }
+  constructor(private postService: PostHighService, private userLowService: UserLowService, private followLowService : FollowLowService) { }
   
   
 
@@ -14,7 +15,7 @@ export class HomePageService {
     const listOfFollowingUsersId = followingUsersId.map((x) => x.followingId)
     const result = (await this.postService.getPostsByUsersId(listOfFollowingUsersId, dto.limit, dto.startTime))
     const posts = result.posts
-    const homePagePosts = await Promise.all(posts.map(async (x) => ({ id: x.id, familyName: await this.userService.getFamilyNameById(x.userId), userName: (await this.userService.getUserById(x.userId)).username ,tags: x.tags, photos: x.photos })))
+    const homePagePosts = await Promise.all(posts.map(async (x) => ({ id: x.id, familyName: await this.userLowService.getFamilyNameById(x.userId), userName: (await this.userLowService.getUserById(x.userId)).username ,tags: x.tags, photos: x.photos })))
     return {
       posts: homePagePosts,
       nextOffset: result.nextOffset,
