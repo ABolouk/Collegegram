1c1635490f99c7b5f41e15a5836ccee757c82d1a
ABolouk <amirhosseinbolouk@gmail.com>
2023-10-06 02:25:22 +0330

    feat: Change getNotificationsByUserId

--- src/modules/notification/notification.repository.ts
+++ src/modules/notification/notification.repository.ts
@@ -1,16 +1,20 @@
 import { z } from "zod";
-import { DataSource, Repository } from "typeorm";
+import { DataSource, LessThan, Repository } from "typeorm";
 import { NotificationEntity } from "./entity/notification.entity";
 import { NotificationId } from "./model/notification-id";
-import { NotificationInterface } from "./model/notification";
+import { CreateNotificationInterface, NotificationInterface } from "./model/notification";
 import { zodNotificationDao } from "./dao/notification.dao";
-import {UserId} from "../user/model/user.id";
+import { GetNotificationsDto } from "./dto/get-notifications.dto";
 
 export class NotificationRepository {
     private notificationRepo: Repository<NotificationEntity>;
 
     constructor(private appDataSource: DataSource) {
         this.notificationRepo = appDataSource.getRepository(NotificationEntity);
+    }
+
+    async create(notification: CreateNotificationInterface) {
+        return zodNotificationDao.parse(await this.notificationRepo.save(notification));
     }
 
     async getNotificationById(id: NotificationId): Promise<NotificationInterface | null> {
@@ -20,8 +24,21 @@ export class NotificationRepository {
         }).then((x) => z.nullable(zodNotificationDao).parse(x));
     }
 
-    async getNotificationsByUserId(id: UserId): Promise<NotificationInterface[]> {
-        return await this.notificationRepo.findBy({interactedUserId: id})
-            .then(notifications => notifications.map(x => zodNotificationDao.parse(x)))
+    async getNotificationsByUserId(dto: GetNotificationsDto) {
+        const [notifications, count] = await this.notificationRepo.findAndCount({
+            // relations: ["posts", "comments"],
+            where: {
+                interactedUserId: dto.userId,
+                createdAt: LessThan(dto.startTime),
+            },
+            order: { createdAt: 'desc' },
+            take: dto.limit,
+        })
+        const hasMore = count > dto.limit;
+        return {
+            notifications: notifications.map(x => zodNotificationDao.parse(x)),
+            nextOffset: notifications.length !== 0 ? notifications[notifications.length - 1].createdAt : new Date(),
+            hasMore: hasMore,
+        }
     }
 }
