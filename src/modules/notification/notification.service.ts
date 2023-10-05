c7311b7a9ed62aeb4aee899a56c4a591d963dd2f
ABolouk <amirhosseinbolouk@gmail.com>
2023-10-06 02:25:01 +0330

    feat: Add event listener to Notification service

--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -1,12 +1,31 @@
-import {NotificationRepository} from "./notification.repository";
-import {NotificationInterface} from "./model/notification";
-import {User} from "../user/model/user";
+import { NotificationRepository } from "./notification.repository";
+import { CreateNotificationInterface } from "./model/notification";
+import { likeEventEmmmiter } from "../../data/event-handling";
+import { PostId } from "../post/model/post-id";
+import { UserId } from "../user/model/user.id";
+import { LikeNotification, LikeNotificationType } from "./model/like-notification";
+import { GetNotificationsDto } from "./dto/get-notifications.dto";
+import { PostService } from "../post/post.service";
 
 export class NotificationService {
-    constructor(private notificationRepository: NotificationRepository) {
+    constructor(
+        private notificationRepository: NotificationRepository,
+        private postService: PostService,
+    ) {
+        likeEventEmmmiter.on('like', async (userId: UserId, postId: PostId) => {
+            const interactingUserId = userId;
+            const interactedUserId = await this.postService.getAuthorById(postId);
+            const newNotification: CreateNotificationInterface = {
+                interactingUserId: interactingUserId,
+                interactedUserId: interactedUserId,
+                type: LikeNotification as LikeNotificationType,
+                postId: postId,
+            }
+            await this.notificationRepository.create(newNotification);
+        })
     }
 
-    async getMyNotifications(user: User): Promise<NotificationInterface[]> {
-        return await this.notificationRepository.getNotificationsByUserId(user.id);
+    async getMyNotifications(dto: GetNotificationsDto) {
+        return await this.notificationRepository.getNotificationsByUserId(dto);
     }
 }
