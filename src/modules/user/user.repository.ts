6de1901f18077db5cf41af8cebb29a462a5dfcd3
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-06 21:04:40 +0330

    feat : add method to get getUsersNotFollowingAndNotBlocked (idk better name)

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,68 +1,88 @@
-import { DataSource, Repository } from "typeorm";
-import { UserEntity } from "./entity/user.entity";
-import { createUserInterface, updateUser, User } from "./model/user";
-import { UserId } from "./model/user.id";
-import { UserName } from "./model/user.username";
-import { Email } from "./model/user.email";
-import { zodUserDao } from "./dao/user.dao";
-import { z } from "zod";
+import {DataSource, In, LessThan, Not, Repository} from "typeorm";
+import {UserEntity} from "./entity/user.entity";
+import {createUserInterface, updateUser, User} from "./model/user";
+import {UserId} from "./model/user.id";
+import {UserName} from "./model/user.username";
+import {Email} from "./model/user.email";
+import {zodUserDao} from "./dao/user.dao";
+import {z} from "zod";
 import {seedUser} from "../../seed-user";
+import {UserFollowingsId} from "../follow/model/follow";
+import {zodExploreUserDao} from "../post/dao/explore.dao";
 
 export class UserRepository {
-	private userRepo: Repository<UserEntity>;
-	constructor(appDataSource: DataSource) {
-		this.userRepo = appDataSource.getRepository(UserEntity);
-		seedUser();
-	}
+    private userRepo: Repository<UserEntity>;
 
-	async isUniqueUserName(username: UserName): Promise<UserName.Unique | null> {
-		const user = await this.findByEmailOrUsername(username)
-		if (user === null) {
-			return username as UserName.Unique
-		}
-		return null
-	}
+    constructor(appDataSource: DataSource) {
+        this.userRepo = appDataSource.getRepository(UserEntity);
+        seedUser();
+    }
 
-	async findByEmail(email: Email): Promise<User | null> {
-		return this.userRepo.findOneBy({ email }).then((x) => z.nullable(zodUserDao).parse(x))
-	}
+    async isUniqueUserName(username: UserName): Promise<UserName.Unique | null> {
+        const user = await this.findByEmailOrUsername(username)
+        if (user === null) {
+            return username as UserName.Unique
+        }
+        return null
+    }
 
-	async isUniqueEmail(email: Email): Promise<Email.Unique | null> {
-		const user = await this.findByEmail(email)
-		if (user === null) {
-			return email as Email.Unique
-		}
-		return null;
-	}
+    async findByEmail(email: Email): Promise<User | null> {
+        return this.userRepo.findOneBy({email}).then((x) => z.nullable(zodUserDao).parse(x))
+    }
 
-	async findByEmailOrUsername(data: Email | UserName): Promise<User | null> {
-		return this.userRepo
-			.findOneBy([{ email: data }, { username: data }])
-			.then((x) => z.nullable(zodUserDao).parse(x));
-	}
+    async isUniqueEmail(email: Email): Promise<Email.Unique | null> {
+        const user = await this.findByEmail(email)
+        if (user === null) {
+            return email as Email.Unique
+        }
+        return null;
+    }
+
+    async findByEmailOrUsername(data: Email | UserName): Promise<User | null> {
+        return this.userRepo
+            .findOneBy([{email: data}, {username: data}])
+            .then((x) => z.nullable(zodUserDao).parse(x));
+    }
 
 
+    async findById(id: UserId): Promise<User | null> {
+        return this.userRepo.findOneBy({id}).then((x) =>
+            z.nullable(zodUserDao).parse(x)
+        )
+    }
 
-	async findById(id: UserId): Promise<User | null> {
-		return this.userRepo.findOneBy({ id }).then((x) =>
-			z.nullable(zodUserDao).parse(x)
-		)
-	}
+    //FIXME: this is not a good way to do this
+    updatePasswordById(id: UserId, password: string) {
+        this.userRepo.update(
+            {id: id},
+            {password: password},
+        )
+    }
 
-	//FIXME: this is not a good way to do this
-	updatePasswordById(id: UserId, password: string) {
-		this.userRepo.update(
-			{ id: id },
-			{ password: password },
-		)
-	}
+    //FIXME: this is not a good way to do this
+    async updateUser(userId: UserId, user: updateUser) {
+        await this.userRepo.update({id: userId}, user)
+    }
 
-	//FIXME: this is not a good way to do this
-	async updateUser(userId: UserId, user: updateUser) {
-		await this.userRepo.update({ id: userId }, user)
-	}
+    async createUser(user: createUserInterface) {
+        return this.userRepo.save(user)
+    }
 
-	async createUser(user: createUserInterface){
-		return this.userRepo.save(user)
-	}
+    async getUsersNotFollowedNotblocked(blockedUsersId: UserId[], blockerUsersId: UserId[], following: UserFollowingsId , limit : number , startTime : Date) {
+        const [usersNotFollowedNotBlocked , count] = await this.userRepo.findAndCount({
+            where: [
+                {id: Not(In(following.map((x) => x.followingId)))},
+                {id: Not(In(blockedUsersId))},
+                {id: Not(In(blockerUsersId))},
+                {createdAt : LessThan(startTime)}
+            ],
+            order: {
+                createdAt: "DESC",
+            },
+            take : limit,
+        });
+        const hasMore = count > limit
+        const users = usersNotFollowedNotBlocked.map((x) => zodExploreUserDao.parse(x))
+        return {users , hasMore}
+    }
 }
