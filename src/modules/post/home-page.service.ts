8871ad836475d0af769f93ecc8ea0c2c25b28e4d
sara yahyazadeh <sarayahyazadeh@saras-MacBook-Air.local>
2023-10-06 23:02:12 +0330

    feat: isLiked, isBookmarked and counts add to homepage posts

--- src/modules/post/home-page.service.ts
+++ src/modules/post/home-page.service.ts
@@ -1,21 +1,25 @@
 import { PostHighService } from "./post.high.service";
-import { UserHighService } from "../user/user.high.service";
 import { homePageDtoType } from "./dto/home-page.dto";
-import { FollowHighService } from "../follow/follow.high.service";
-import {FollowLowService} from "../follow/follow.low.service";
-import {UserLowService} from "../user/user.low.service";
+import { FollowLowService } from "../follow/follow.low.service";
+import { UserLowService } from "../user/user.low.service";
+import { LikeLowService } from "./like/like.low.service";
+import { BookmarkService } from "../bookmark/book-mark.service";
 
 export class HomePageService {
-  constructor(private postService: PostHighService, private userLowService: UserLowService, private followLowService : FollowLowService) { }
-  
-  
+  constructor(private postService: PostHighService, private userLowService: UserLowService, private followLowService: FollowLowService, private likeService: LikeLowService, private bookmarkService: BookmarkService) { }
+
+
 
   async getHome(dto: homePageDtoType) {
     const followingUsersId = await this.followLowService.getFollowingsIdByUserId(dto.userId)
     const listOfFollowingUsersId = followingUsersId.map((x) => x.followingId)
     const result = (await this.postService.getPostsByUsersId(listOfFollowingUsersId, dto.limit, dto.startTime))
     const posts = result.posts
-    const homePagePosts = await Promise.all(posts.map(async (x) => ({ id: x.id, familyName: await this.userLowService.getFamilyNameById(x.userId), userName: (await this.userLowService.getUserById(x.userId)).username ,tags: x.tags, photos: x.photos })))
+    const homePagePosts = await Promise.all(posts
+      .map(async (x) => ({
+        id: x.id, familyName: await this.userLowService.getFamilyNameById(x.userId), userName: (await this.userLowService.getUserById(x.userId)).username, tags: x.tags, photos: x.photos, isLiked: await this.likeService.isLiked({ userId: dto.userId, postId: x.id }), isBookmarked: await this.bookmarkService.isBookmarked(dto.userId, x.id),
+        likeCount: x.likeCount, bookmarkCount: x.bookmarkCount, commentCount: x.commentCount
+      })))
     return {
       posts: homePagePosts,
       nextOffset: result.nextOffset,
