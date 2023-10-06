228cd5aa5c08e681edeebd63e2a8221fa3033a27
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-06 21:02:22 +0330

    feat : add explore posts and users dao

---
+++ src/modules/post/dao/explore.dao.ts
@@ -0,0 +1,28 @@
+import {z} from "zod";
+import {PostId} from "../model/post-id";
+import {ExplorePostsDao, ExploreUserDao} from "../model/explore";
+import {UserName} from "../../user/model/user.username";
+import {zodFirstName} from "../../user/model/user.firstName";
+import {lastName, zodLastName} from "../../user/model/user.lastName";
+import {WholeNumber} from "../../../data/whole-number";
+import {UserId} from "../../user/model/user.id";
+
+export const zodExplorePostDao = z.object({
+    id: PostId.zod,
+    photos: z.array(z.string())
+}).transform((x) => {
+    return {
+        id: x.id,
+        photo: x.photos[0]
+    } as ExplorePostsDao
+})
+
+export const zodExploreUserDao = z.object({
+    id: UserId.zod,
+    userName: UserName.zod,
+    firstName: z.nullable(zodFirstName),
+    lastName: z.nullable(zodLastName),
+    avatar: z.string(),
+    followerCount: WholeNumber.zod,
+    createdAt: z.date()
+}).transform((x): ExploreUserDao => (x))
