a4188470f65a5622343134469bcf29cf2d23ba9b
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-07 18:30:34 +0330

    refactor : make it clean

--- src/modules/bookmark/entity/book-mark.entity.ts
+++ src/modules/bookmark/entity/book-mark.entity.ts
@@ -1,32 +1,40 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { UserId } from "../../user/model/user.id";
-import { UserEntity } from "../../user/entity/user.entity";
-import { PostId } from "../../post/model/post-id";
-import { PostEntity } from "../../post/entity/post.entity";
-import { BookMarkId } from "../model/book-mark-id";
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+import {UserId} from "../../user/model/user.id";
+import {UserEntity} from "../../user/entity/user.entity";
+import {PostId} from "../../post/model/post-id";
+import {PostEntity} from "../../post/entity/post.entity";
+import {BookMarkId} from "../model/book-mark-id";
 
 @Entity("bookmark")
 export class BookmarkEntity {
 
-  @PrimaryGeneratedColumn()
-  id !: BookMarkId
+    @PrimaryGeneratedColumn()
+    id !: BookMarkId
 
-  @Column()
-  userId !: UserId
+    @Column()
+    userId !: UserId
 
-  @ManyToOne(() => UserEntity)
-  user!: UserEntity
+    @ManyToOne(() => UserEntity)
+    user!: UserEntity
 
-  @Column()
-  postId !: PostId
+    @Column()
+    postId !: PostId
 
-  @ManyToOne(() => PostEntity)
-  post!: PostEntity
+    @ManyToOne(() => PostEntity)
+    post!: PostEntity
 
-  @CreateDateColumn()
-  createdAt!: Date;
+    @CreateDateColumn()
+    createdAt!: Date;
 
-  @UpdateDateColumn()
-  updatedAt!: Date;
+    @UpdateDateColumn()
+    updatedAt!: Date;
 
 }
