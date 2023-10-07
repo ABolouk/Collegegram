206c787e7dee4792ef9e4f4485a481e5df7123cd
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-07 11:21:00 +0330

    feat : add notification events listeners and makers

--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -1,11 +1,24 @@
-import { NotificationRepository } from "./notification.repository";
-import { CreateNotificationInterface } from "./model/notification";
-import { likeEventEmmmiter } from "../../data/event-handling";
-import { PostId } from "../post/model/post-id";
-import { UserId } from "../user/model/user.id";
-import { LikeNotification, LikeNotificationType } from "./model/like-notification";
-import { GetNotificationsDto } from "./dto/get-notifications.dto";
-import { PostService } from "../post/post.service";
+import {NotificationRepository} from "./notification.repository";
+import {CreateNotificationInterface} from "./model/notification";
+import {
+    acceptFollowRequestEventEmmmiter,
+    commentEventEmmmiter,
+    followEventEmmmiter,
+    followRequestEventEmmmiter,
+    likeEventEmmmiter
+} from "../../data/event-handling";
+import {PostId} from "../post/model/post-id";
+import {UserId} from "../user/model/user.id";
+import {LikeNotification, LikeNotificationType} from "./model/like-notification";
+import {GetNotificationsDto} from "./dto/get-notifications.dto";
+import {PostService} from "../post/post.service";
+import {CommentNotification, CommentNotificationType} from "./model/comment-notification";
+import {FollowNotification, FollowNotificationType} from "./model/follow-notification";
+import {
+    FollowRequestAcceptedNotification, FollowRequestAcceptedNotificationType,
+    FollowRequestPendingNotification,
+    FollowRequestPendingNotificationType
+} from "./model/follow-request-notification";
 
 export class NotificationService {
     constructor(
@@ -13,19 +26,82 @@ export class NotificationService {
         private postService: PostService,
     ) {
         likeEventEmmmiter.on('like', async (userId: UserId, postId: PostId) => {
-            const interactingUserId = userId;
-            const interactedUserId = await this.postService.getAuthorById(postId);
-            const newNotification: CreateNotificationInterface = {
-                interactingUserId: interactingUserId,
-                interactedUserId: interactedUserId,
-                type: LikeNotification as LikeNotificationType,
-                postId: postId,
-            }
-            await this.notificationRepository.create(newNotification);
+            await this.makeLikeNotification(userId, postId);
+        })
+        commentEventEmmmiter.on('comment', async (userId: UserId, postId: PostId) => {
+            await this.makeCommentNotification(userId, postId);
+        })
+        followEventEmmmiter.on('follow', async (followerId: UserId, followingId: UserId) => {
+            await this.makeFollowNotification(followerId, followingId);
+        })
+        followRequestEventEmmmiter.on('followRequest', async (followerId: UserId, followingId: UserId) => {
+            await this.makeFollowRequstNotification(followerId, followingId);
+        })
+        acceptFollowRequestEventEmmmiter.on('acceptFollowRequest', async (followerId: UserId, followingId: UserId) => {
+            await this.makeAcceptFollowNotification(followerId, followingId);
         })
     }
 
     async getMyNotifications(dto: GetNotificationsDto) {
         return await this.notificationRepository.getNotificationsByUserId(dto);
     }
+
+    async makeLikeNotification(userId: UserId, postId: PostId) {
+        const interactingUserId = userId;
+        const interactedUserId = await this.postService.getAuthorById(postId);
+        const newNotification: CreateNotificationInterface = {
+            interactingUserId: interactingUserId,
+            interactedUserId: interactedUserId,
+            type: LikeNotification as LikeNotificationType,
+            postId: postId,
+        }
+        await this.notificationRepository.create(newNotification);
+    }
+
+    async makeCommentNotification(userId: UserId, postId: PostId) {
+        const interactingUserId = userId;
+        const interactedUserId = await this.postService.getAuthorById(postId);
+        const newNotification: CreateNotificationInterface = {
+            interactingUserId: interactingUserId,
+            interactedUserId: interactedUserId,
+            type: CommentNotification as CommentNotificationType,
+            postId: postId,
+        }
+        await this.notificationRepository.create(newNotification);
+    }
+
+    async makeFollowNotification(followerId: UserId, followingId: UserId) {
+        const interactingUserId = followerId;
+        const interactedUserId = followingId;
+        const newNotification: CreateNotificationInterface = {
+            interactingUserId: interactingUserId,
+            interactedUserId: interactedUserId,
+            type: FollowNotification as FollowNotificationType,
+        }
+        await this.notificationRepository.create(newNotification);
+    }
+
+    async makeFollowRequstNotification(followerId: UserId, followingId: UserId) {
+        const interactingUserId = followerId;
+        const interactedUserId = followingId;
+        const newNotification: CreateNotificationInterface = {
+            interactingUserId: interactingUserId,
+            interactedUserId: interactedUserId,
+            type: FollowRequestPendingNotification as FollowRequestPendingNotificationType,
+        }
+        await this.notificationRepository.create(newNotification);
+    }
+
+    async makeAcceptFollowNotification(followerId: UserId, followingId: UserId) {
+        const interactingUserId = followerId;
+        const interactedUserId = followingId;
+        const newNotification: CreateNotificationInterface = {
+            interactingUserId: interactingUserId,
+            interactedUserId: interactedUserId,
+            type: FollowRequestAcceptedNotification as FollowRequestAcceptedNotificationType,
+        }
+        await this.notificationRepository.create(newNotification);
+    }
+
+
 }
