fd19b0a04d25d308b4c9b3a441cd27010a528129
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-07 18:42:53 +0330

    feat : add blockAction

--- src/modules/post/comment/comment.repository.ts
+++ src/modules/post/comment/comment.repository.ts
@@ -1,12 +1,13 @@
-import { Repository, DataSource, LessThan } from "typeorm";
-import { CommentEntity } from "./entity/comment.entity";
-import { CommentInterface } from "./model/comment";
-import { zodCommentDao } from "./dao/create-comment.dao";
-import { PostEntity } from "../entity/post.entity";
-import { UserId } from "../../user/model/user.id";
-import { PostId } from "../model/post-id";
-import { z } from "zod"
-import { zodGetCommentDao } from "./dao/get-comment.dao";
+import {Repository, DataSource, LessThan, In} from "typeorm";
+import {CommentEntity} from "./entity/comment.entity";
+import {CommentInterface} from "./model/comment";
+import {zodCommentDao} from "./dao/create-comment.dao";
+import {PostEntity} from "../entity/post.entity";
+import {UserId} from "../../user/model/user.id";
+import {PostId} from "../model/post-id";
+import {z} from "zod"
+import {zodGetCommentDao} from "./dao/get-comment.dao";
+import {LikeEntity} from "../like/entity/like.entity";
 
 export class CommentRepository {
     private commentRepo: Repository<CommentEntity>;
@@ -20,8 +21,8 @@ export class CommentRepository {
             const savedComment = await this.commentRepo.save(comment);
             const postRepo = manager.getRepository(PostEntity);
             await postRepo.update(
-                { id: comment.postId },
-                { commentCount: () => "commentCount + 1" }
+                {id: comment.postId},
+                {commentCount: () => "commentCount + 1"}
             );
             return zodCommentDao.parse(savedComment);
         });
@@ -34,14 +35,52 @@ export class CommentRepository {
                     postId: postId,
                     createdAt: LessThan(startTime)
                 },
-                order: { createdAt: 'desc' },
+                order: {createdAt: 'desc'},
                 take: limit
 
             }
         )
         const resultComments = z.nullable(zodGetCommentDao).parse(comments)
         const hasMore = count > limit
-        return { comments: resultComments, hasMore: hasMore }
+        return {comments: resultComments, hasMore: hasMore}
     }
 
+    async blockAction(blockerId: UserId, blockedId: UserId) {
+        const comment = await this.commentRepo.find({
+            select: {
+                userId: true,
+                postId: true,
+                post: {
+                    userId: true,
+                },
+            },
+            relations: {
+                post: true,
+            },
+            where: [{
+                userId: blockedId,
+                post: {
+                    userId: blockerId,
+                }
+            }, {
+                userId: blockerId,
+                post: {
+                    userId: blockedId,
+                }
+            },
+            ],
+        });
+        await this.appDataSource.manager.transaction(async manager => {
+            const postRepo = manager.getRepository(PostEntity);
+            const likeRepo = manager.getRepository(CommentEntity);
+            const newLike = await likeRepo.delete(
+                {postId: In(comment.map(x => x.postId))}
+            );
+            await postRepo.update(
+                {id: In(comment.map(x => x.postId))},
+                {likeCount: () => "commentCount - 1"}
+            );
+        });
+        return {status: "blocked"}
+    }
 }
