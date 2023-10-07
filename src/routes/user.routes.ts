29211259646fca1b83ed3a8945a8b3183c1eb6d7
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-07 12:47:55 +0330

    feat : add addCloseFriend route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,28 +1,30 @@
-import { Router } from "express";
-import { loginDto } from '../modules/user/dto/login.dto';
-import { handleExpresss } from "../utility/handle-express";
-import { UserHighService } from '../modules/user/user.high.service';
-import { forgetPasswordDto } from "../modules/user/dto/forget-password.dto";
-import { signupDto } from "../modules/user/dto/signup.dto";
-import { loginMiddle } from "../login.middleware";
-import { editProfile } from "../modules/user/dto/edit-profile.dto";
-import { followDto } from '../modules/follow/dto/follow.dto';
-import { unfollowDto } from "../modules/follow/dto/unfollow.dto";
-import { JwtService } from "../modules/jwt/jwt.service";
-import { blockDto } from "../modules/block/dto/block.dto";
-import { getUserDto } from "../modules/user/dto/get.user.dto";
-import { FollowHighService } from "../modules/follow/follow.high.service";
-import { acceptFollowReq } from "../modules/follow/dto/followreq.accept.dto";
-import { rejectFollowReq } from "../modules/follow/dto/followreq.reject.dto";
-import { uploadAvatarMinIO } from "../utility/multer";
-import { UserLowService } from "../modules/user/user.low.service";
-import { SessionLowService } from "../modules/user/session.low.service";
-import { BlockHighService } from "../modules/block/block.high.service";
+import {Router} from "express";
+import {loginDto} from '../modules/user/dto/login.dto';
+import {handleExpresss} from "../utility/handle-express";
+import {UserHighService} from '../modules/user/user.high.service';
+import {forgetPasswordDto} from "../modules/user/dto/forget-password.dto";
+import {signupDto} from "../modules/user/dto/signup.dto";
+import {loginMiddle} from "../login.middleware";
+import {editProfile} from "../modules/user/dto/edit-profile.dto";
+import {followDto} from '../modules/follow/dto/follow.dto';
+import {unfollowDto} from "../modules/follow/dto/unfollow.dto";
+import {JwtService} from "../modules/jwt/jwt.service";
+import {blockDto} from "../modules/block/dto/block.dto";
+import {getUserDto} from "../modules/user/dto/get.user.dto";
+import {FollowHighService} from "../modules/follow/follow.high.service";
+import {acceptFollowReq} from "../modules/follow/dto/followreq.accept.dto";
+import {rejectFollowReq} from "../modules/follow/dto/followreq.reject.dto";
+import {uploadAvatarMinIO} from "../utility/multer";
+import {UserLowService} from "../modules/user/user.low.service";
+import {SessionLowService} from "../modules/user/session.low.service";
+import {BlockHighService} from "../modules/block/block.high.service";
+import {closeFriendDto} from "../modules/user/closeFriend/dto/close-friend.dto";
+import {CloseFriendHighService} from "../modules/user/closeFriend/close-Friend.high.service";
 
 export const resetPasswordRoute = "reset_password"
 
 
-export const makeUserRouter = (userHighService: UserHighService, sessionLowService: SessionLowService, userLowService: UserLowService, jwtService: JwtService, followHighService: FollowHighService, blockHighService: BlockHighService) => {
+export const makeUserRouter = (userHighService: UserHighService, sessionLowService: SessionLowService, userLowService: UserLowService, jwtService: JwtService, followHighService: FollowHighService, blockHighService: BlockHighService, closeFriendHighService : CloseFriendHighService) => {
     const app = Router();
     app.post("/login", (req, res) => {
         const dto = loginDto.parse(req.body);
@@ -49,8 +51,8 @@ export const makeUserRouter = (userHighService: UserHighService, sessionLowServi
     })
 
     app.post(`/${resetPasswordRoute}/:userId/:token`, async (req, res) => {
-        const { userId, token } = req.params;
-        const { password1, password2 } = req.body;
+        const {userId, token} = req.params;
+        const {password1, password2} = req.body;
         handleExpresss(res, () => userHighService.resetPassword(userId, token, password1, password2));
     })
     app.put("/editProfile", loginMiddle(userLowService, sessionLowService), uploadAvatarMinIO.single('avatar'), (req, res) => {
@@ -64,46 +66,53 @@ export const makeUserRouter = (userHighService: UserHighService, sessionLowServi
     app.post("/follow", loginMiddle(userLowService, sessionLowService), (req, res) => {
         const follower = req.user.id;
         const following = req.body.userName
-        const dto = followDto.parse({ follower, following })
+        const dto = followDto.parse({follower, following})
         handleExpresss(res, () => followHighService.createFollowRelation(dto));
     });
     app.post("/unfollow", loginMiddle(userLowService, sessionLowService), (req, res) => {
         const follower = req.user.id;
         const following = req.body.userName
-        const dto = unfollowDto.parse({ follower, following })
+        const dto = unfollowDto.parse({follower, following})
         handleExpresss(res, () => followHighService.unfollow(dto));
     });
     app.post("/acceptFollowRequest", loginMiddle(userLowService, sessionLowService), (req, res) => {
         const follower = req.user.id;
         const following = req.body.userName
-        const dto = acceptFollowReq.parse({ follower, following })
+        const dto = acceptFollowReq.parse({follower, following})
         handleExpresss(res, () => followHighService.acceptFollowRequest(dto));
     });
     app.post("/rejectFollowRequest", loginMiddle(userLowService, sessionLowService), (req, res) => {
         const follower = req.user.id;
         const following = req.body.userName
-        const dto = rejectFollowReq.parse({ follower, following })
+        const dto = rejectFollowReq.parse({follower, following})
         handleExpresss(res, () => followHighService.rejectFollowRequest(dto));
     });
     app.post("/cancelFollowRequest", loginMiddle(userLowService, sessionLowService), (req, res) => {
         const follower = req.user.id;
         const following = req.body.userName
-        const dto = followDto.parse({ follower, following })
+        const dto = followDto.parse({follower, following})
         handleExpresss(res, () => followHighService.cancelFollowRequest(dto));
     });
 
     app.post("/block", loginMiddle(userLowService, sessionLowService), (req, res) => {
         const follower = req.user.id;
         const following = req.body.userName
-        const dto = blockDto.parse({ follower, following })
+        const dto = blockDto.parse({follower, following})
         handleExpresss(res, () => blockHighService.block(dto))
     })
 
     app.post("/unblock", loginMiddle(userLowService, sessionLowService), (req, res) => {
         const follower = req.user.id;
         const following = req.body.userName
-        const dto = blockDto.parse({ follower, following })
+        const dto = blockDto.parse({follower, following})
         handleExpresss(res, () => blockHighService.unblock(dto))
+    })
+
+    app.post("/addCloseFriend", loginMiddle(userLowService, sessionLowService), (req, res) => {
+        const userId = req.user.id;
+        const userName = req.body.userName;
+        const dto = closeFriendDto.parse({userId, userName})
+        handleExpresss(res, () => closeFriendHighService.addCloseFriend(dto))
     })
 
     return app
