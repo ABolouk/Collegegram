7caa25cece9d27827e02e989d2af8b3a40e84008
Ali-Mansouri03 <108889686+Ali-Mansouri03@users.noreply.github.com>
2023-10-07 11:23:11 +0330

    feat : add zodFollowReqDao

--- src/modules/follow/dao/follow.dao.ts
+++ src/modules/follow/dao/follow.dao.ts
@@ -2,6 +2,9 @@ import { z } from "zod";
 import { FollowId } from "../model/follow.id";
 import { followDao, followIdDao, UserFollowingsId } from "../model/follow";
 import { UserId } from "../../user/model/user.id";
+import {followReqDao} from "../model/follow.request";
+import {FollowReqStatus} from "../model/follow.req.status";
+import {FollowReqId} from "../model/follow.req.id";
 
 export const zodFollowIdDao = z.object({
     id: FollowId.zod,
@@ -17,3 +20,10 @@ export const zodFollowRellDao = z.object({
 export const zodFollowingsId = z.array(z.object({
     followingId: UserId.zod
 })).transform((x): UserFollowingsId => x)
+
+export const zodFollowReqDao = z.object({
+    id: FollowReqId.zod,
+    followingId: UserId.zod,
+    followerId: UserId.zod,
+    status: FollowReqStatus.zod
+}).transform((x): followReqDao => x)
